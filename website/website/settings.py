93140316049ecf20addb408bcbde7e83635d019b
saeid186 <saeid186nasiri@gmail.com>
2022-12-29 23:46:36 +0330

    update requirements and change database to MySql

--- website/website/settings.py
+++ website/website/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sevenet',
+        'USER': 'sevenet',
+        'PASSWORD': 'sevenet@123',
+        'HOST': '31.40.2.192',
+        'PORT': '3306',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # DATABASES = {
 #     'default': {
